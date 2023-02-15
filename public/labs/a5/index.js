1674a306ef8c159d91018a4be72ad2a7a08c3918
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-15 15:43:49 -0500

    a5 lab through 2.8

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -180,3 +180,23 @@ const oddNumbers = numberArray1
 console.log(numbersGreaterThan2);
 console.log(evenNumbers);
 console.log(oddNumbers);
+
+
+console.log('Template strings');
+const five = 2 + 3;
+const result1 = "2 + 3 = " + five;
+console.log(result1);
+
+const result2 = `2 + 3 = ${2 + 3}`;
+console.log(result2);
+
+const username = "alice";
+const greeting1 = `Welcome home ${username}`;
+console.log(greeting1);
+
+loggedIn = false;
+const greeting2 = `Logged in: ${loggedIn ? "Yes" : "No"}`;
+console.log(greeting2)
+
+
+
