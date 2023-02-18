6ce3a2cbdd3a44ba6d56a2b3fe0a7e5bb6adc6c8
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-18 18:38:17 -0500

    change icon to 1x for whoToFollow list items

--- public/tuiter/WhoToFollowList/WhoToFollowListItem.js
+++ public/tuiter/WhoToFollowList/WhoToFollowListItem.js
@@ -1,5 +1,5 @@
 
-const potentialListItem = {
+const exampleListItem = {
     avatarIcon: '../../images/virgin_logo.svg',
     userName: 'Virgin Galactic',
     handle: 'virgingalactic',
@@ -18,8 +18,8 @@ const WhoToFollowListItem = who => {
               <div class="col-6 col-xl-7">
                 <div class="fw-bold">
                 <span>${userName}</span>
-                  <span class="fa-stack small">
-                    <i class="fa-solid fa-certificate fa-stack-2x"></i>
+                  <span class="fa-stack">
+                    <i class="fa-solid fa-certificate fa-stack-1x"></i>
                     <i class="fa-solid fa-check fa-stack-1x fa-inverse"></i>
                   </span>
                 </div>
@@ -38,8 +38,8 @@ const WhoToFollowListItem = who => {
 export default WhoToFollowListItem;
 
 
-function renderNavBar() {
-    $('#wtfli').append(WhoToFollowListItem(potentialListItem));
+function renderWhoToFollowListItem() {
+    $('#wtfli').append(WhoToFollowListItem(exampleListItem));
 
 }
-$(renderNavBar)
+$(renderWhoToFollowListItem)
