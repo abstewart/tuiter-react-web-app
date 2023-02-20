2860f5d51f1429996a9222a13cfc4f2bbfaf932c
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-20 18:16:15 -0500

    whotofollow list, styling still broken

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,15 +1,10 @@
 import Nav from "../nav";
-import NavigationSidebar from "./navigation-sidebar";
-import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 function Tuiter() {
-  const a = 10;
   return (
       <div>
         <Nav/>
-        <FontAwesomeIcon icon={['fa', 'certificate']}/>
         <h1>Tuiter</h1>
       </div>
   )
---
+++ src/tuiter/who-to-follow-list/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import WhoToFollowListItem from "./who-to-follow-list-item";
+import whoArray from './who.json'
+
+
+const WhoToFollowList = () => {
+  return(
+      <div className="list-group">
+        <li className="list-group-item"><b>Who to follow</b></li>
+          {
+            whoArray.map(who =>
+            <WhoToFollowListItem
+            key={who._id}
+            who={who}/>
+            )
+          }
+      </div>
+  );
+};
+
+export default WhoToFollowList;
