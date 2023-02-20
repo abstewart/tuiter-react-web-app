dd701fa6ba54fefa90787293706035b9c15a2437
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-20 15:55:19 -0500

    a6 lab upto 2.7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
 
 function App() {
   return (
       <div className="container">
-        <h1>Hello World!</h1>
+        <HelloWorld/>
         <Labs/>
+        <Tuiter/>
       </div>
   );
 }
