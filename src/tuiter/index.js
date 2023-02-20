2b7844a3735223a9cd02745a7b28b324008ac8a6
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-20 18:32:33 -0500

    explore component placeholder

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -2,6 +2,7 @@ import Nav from "../nav";
 import WhoToFollowList from "./who-to-follow-list";
 import PostSummaryItem from "./post-summary-list/post-summary-item";
 import PostSummaryList from "./post-summary-list";
+import Explore from "./explore";
 
 
 function Tuiter() {
@@ -9,8 +10,7 @@ function Tuiter() {
       <div>
         <Nav/>
         <h1>Tuiter</h1>
-        <PostSummaryList/>
-        <WhoToFollowList/>
+        <Explore/>
       </div>
   )
 }
