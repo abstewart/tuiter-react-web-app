04e47fa9d4f85ce81b5ae8f8c6ad72efdd1fa1cd
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-20 17:55:12 -0500

    fontawesome icon library

---
+++ src/fontawesome.js
@@ -0,0 +1,11 @@
+//import the library
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+//import icons
+import {faCertificate} from "@fortawesome/free-solid-svg-icons";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+    faCertificate,
+    faCheck
+)
