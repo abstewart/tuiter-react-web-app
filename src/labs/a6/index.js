1a7ae33b352eb8a611d058408c133983f053dc3e
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-20 16:23:22 -0500

    a6 lab upto 2.12

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,9 +1,11 @@
 import Classes from "./classes";
+import Styles from "./styles";
 
 function Assignment6() {
   return(
       <div>
         <h1>Assignment 6</h1>
+        <Styles/>
         <Classes/>
       </div>
   )
