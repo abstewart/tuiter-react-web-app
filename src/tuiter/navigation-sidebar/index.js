b257a23223cda59229d123f905a2d0f9656be61e
abstewart <26328673+abstewart@users.noreply.github.com>
2023-02-21 12:49:54 -0500

    use own existing code for navbar component

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -14,35 +14,35 @@ const NavigationSidebar = (
             <FontAwesomeIcon icon="fa-brands fa-twitter" className="fab"/>
             {/*<i className="fab fa-twitter"></i>*/}
           </div>
-          <a href="#" className="list-group-item list-group-item-action">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'home'?'active':''}`}>
             <FontAwesomeIcon icon="fa-house" className="fa-solid fa-fw"/>
             {/*<i className="fa-solid fa-house"></i>*/}
             <span className="d-none d-xl-inline">Home</span></a>
-          <a href="#" className="list-group-item list-group-item-action active">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'explore'?'active':''}`}>
             <FontAwesomeIcon icon="fa-hashtag" className="fa-solid fa-fw"/>
             {/*<i className="fa-solid fa-hashtag"></i>*/}
             <span className="d-none d-xl-inline">Explore</span></a>
-          <a href="#" className="list-group-item list-group-item-action">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'notifications'?'active':''}`}>
             <FontAwesomeIcon icon="fa-bell" className="fa-solid fa-fw"/>
             {/*<i className="fa-solid fa-bell"></i>*/}
             <span className="d-none d-xl-inline">Notifications</span></a>
-          <a href="#" className="list-group-item list-group-item-action">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'messages'?'active':''}`}>
             <FontAwesomeIcon icon="fa-envelope" className="fa-solid fa-fw"/>
             {/*<i className="fa-solid fa-envelope"></i>*/}
             <span className="d-none d-xl-inline">Messages</span></a>
-          <a href="#" className="list-group-item list-group-item-action">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'bookmarks'?'active':''}`}>
             <FontAwesomeIcon icon="fa-bookmark" className="fa-solid fa-fw"/>
             {/*<i className="fa fa-bookmark"></i>*/}
             <span className="d-none d-xl-inline">Bookmarks</span></a>
-          <a href="#" className="list-group-item list-group-item-action">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'lists'?'active':''}`}>
             <FontAwesomeIcon icon="fa-list-ul" className="fa-solid fa-fw"/>
             {/*<i className="fa-solid fa-list-ul"></i>*/}
             <span className="d-none d-xl-inline">Lists</span></a>
-          <a href="#" className="list-group-item list-group-item-action">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'profile'?'active':''}`}>
             <FontAwesomeIcon icon="fa-user" className="fa-solid fa-fw"/>
             {/*<i className="fa-solid fa-user"></i>*/}
             <span className="d-none d-xl-inline">Profile</span></a>
-          <a href="#" className="list-group-item list-group-item-action ps-2">
+          <a href="#" className={`list-group-item list-group-item-action p-2 ${active === 'more'?'active':''}`}>
             <span className="fa-layers fa-fw">
               <FontAwesomeIcon icon="fa-circle" className="fa-solid fa-stack1x"/>
               <FontAwesomeIcon icon="fa-ellipsis" className="fa-solid fa-stack1x fa-inverse"/>
@@ -60,43 +60,6 @@ const NavigationSidebar = (
           </button>
         </div>
       </>
-  )
-  return (
-      <div className="list-group">
-        <a className="list-group-item">Tuiter</a>
-        <a className={`list-group-item
-                    ${active === 'home'?'active':''}`}>
-          Home
-        </a>
-        <a className={`list-group-item
-                    ${active === 'explore'?'active':''}`}>
-          Explore
-        </a>
-        <a className={`list-group-item
-                    ${active === 'notifications'?'active':''}`}>
-          Notifications
-        </a>
-        <a className={`list-group-item
-                    ${active === 'messages'?'active':''}`}>
-          Messages
-        </a>
-        <a className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`}>
-          Bookmarks
-        </a>
-        <a className={`list-group-item
-                    ${active === 'lists'?'active':''}`}>
-          Lists
-        </a>
-        <a className={`list-group-item
-                    ${active === 'profile'?'active':''}`}>
-          Profile
-        </a>
-        <a className={`list-group-item
-                    ${active === 'more'?'active':''}`}>
-          More
-        </a>
-      </div>
   );
 };
 export default NavigationSidebar;
