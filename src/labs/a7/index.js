927463c389607ef9637818b7a67a7f456b9ec416
abstewart <26328673+abstewart@users.noreply.github.com>
2023-03-13 15:37:51 -0400

    a7 up to 2.3

---
+++ src/labs/a7/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "../../nav";
+const Assignment7 = () => {
+  return(
+      <>
+        <h1>Assignment 7</h1>
+      </>
+  );
+};
+export default Assignment7;
